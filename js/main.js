a7f31d68defe5cfe871852fbae78cf9fdfd9d188
Lance <Lance@thelpstory.com>
2015-08-10 07:22:21 -0600

    completed through problem 5

--- js/main.js
+++ js/main.js
@@ -16,13 +16,43 @@ function byTwo(start,stop){
 
 function evenForty(num){
   if(num < 40){
-    return byTwo(40,num);
-  } else {
     return num;
+  } else {
+    return byTwo(40,num);
   }
 }
+
+function oddFortyCount(num){
+  if(num < 40){
+    var array = [];
+    for (var i = 40; i > num; i--) {
+      array.push(i);
+    }
+    return array;
+  } else {
+    return byTwo(41,num);
+  }
+}
+
+function summedDigits(num){
+  var numString = num.toString();
+  var newNum = 0;
+  if(num>0 && num<10000){
+    for (var i = 0; i < numString.length; i++) {
+      debugger;
+      newNum += parseInt(numString[i]);
+    }
+    return newNum;
+  } else {
+    return 'invalid number';
+  }
+}
+
+
 module.exports = {
   largestNumber: largestNumber,
   byTwo: byTwo,
-  evenForty: evenForty
+  evenForty: evenForty,
+  oddFortyCount: oddFortyCount,
+  summedDigits: summedDigits
 };
